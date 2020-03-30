ac43d3dbc9b12b149abbe5e340aee8899b12f593
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-30 04:28:07 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -25,21 +25,3 @@ class Customer
     waiter
   end
 end
-
-# it "returns an Array of Meal instances associated with this customer" do
-#   howard = Customer.new("Howard", 30)
-#   daniel = Customer.new("Daniel", 30)
-#   terrance = Waiter.new("Terrance", 1)
-#   joe = Waiter.new("Joe", 10)
-#   esmery = Waiter.new("Esmery", 2)
-#   andrew = Waiter.new("Andrew", 3)
-#
-#   howard.new_meal(terrance, 15, 2)
-#   howard.new_meal(joe, 15, 4)
-#   howard.new_meal(andrew, 15, 5)
-#   daniel.new_meal(terrance, 20, 1)
-#   daniel.new_meal(esmery, 15, 3)
-#
-#   expect(Meal.all.length).to eq(5)
-#   expect(howard.meals.length).to eq(3)
-#   expect(daniel.meals.length).to eq(2)
