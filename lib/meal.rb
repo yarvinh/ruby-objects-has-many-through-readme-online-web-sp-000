a550baed975c1062d0e8404b9f6d74d5c7899b9b
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-30 04:28:54 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -12,15 +12,3 @@ class Meal
     @@all
   end
 end
-
-# describe "Customer" do
-#   describe "#new_meal" do
-#     it "initializes a meal using the current Customer instance, a provided Waiter instance and a total and tip" do
-#       howard = Customer.new("Howard", 30)
-#       terrance = Waiter.new("Terrance", 1)
-#       howard.new_meal(terrance, 10, 1)
-#
-#       expect(Meal.all.first.waiter).to eq(terrance)
-#       expect(Meal.all.first.customer).to eq(howard)
-#     end
-#   end
