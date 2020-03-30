fc246f65a8c1ad6a73fc87609f3922826496ec7b
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-30 02:59:49 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,7 +11,7 @@ class Customer
   end
 
   def new_meal(waiter,total,tip = 0)
-    Meal.new(self,waiter,total,tip )
+    Meal.new(waiter,self,total,tip )
     # customer = self
     # customer.waiter = waiter
     # customer.total = total
