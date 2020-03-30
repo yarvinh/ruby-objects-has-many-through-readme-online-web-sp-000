8bfdf4d8ec313e77d923398067574ce13d8e9df6
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-30 03:13:49 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,7 +13,7 @@ class Customer
     Meal.new(waiter,self,total,tip )
   end
   def meals
-    
+
   end
 end
 
@@ -24,13 +24,13 @@ end
 #   joe = Waiter.new("Joe", 10)
 #   esmery = Waiter.new("Esmery", 2)
 #   andrew = Waiter.new("Andrew", 3)
-#   
+#
 #   howard.new_meal(terrance, 15, 2)
 #   howard.new_meal(joe, 15, 4)
 #   howard.new_meal(andrew, 15, 5)
 #   daniel.new_meal(terrance, 20, 1)
 #   daniel.new_meal(esmery, 15, 3)
-#   
+#
 #   expect(Meal.all.length).to eq(5)
 #   expect(howard.meals.length).to eq(3)
 #   expect(daniel.meals.length).to eq(2)
